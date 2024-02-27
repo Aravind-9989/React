33c76aa89fc8b8f78d83aa2e49ebbf74b1644b88
Aravind-9989 <shetty2160@gmail.com>
2024-02-27 22:44:24 +0530

    previous 13th task

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
- import Img from './components/functionalComponents/jsx/image';
-import TextComponent from './components/functionalComponents/text/textcomponent';
+//  import Img from './components/functionalComponents/jsx/image';
+// import TextComponent from './components/functionalComponents/text/textcomponent';
 import Designed from './components/functionalComponents/jsx/jsx';
-import Buttons from './components/functionalComponents/props/button';
-import Profile from './components/functionalComponents/props/profile';
+// import Buttons from './components/functionalComponents/props/button';
+// import Profile from './components/functionalComponents/props/profile';
 
 
 
@@ -12,11 +12,11 @@ function App() {
   return (
    <div>
     <h1>Hello</h1>
-<Img/>
- <TextComponent/>
+{/* <Img/>
+ <TextComponent/> */}
  <Designed/>
- <Buttons/>
- <Profile/>
+ {/* <Buttons/>
+ <Profile/> */}
    </div>
   );
 }
